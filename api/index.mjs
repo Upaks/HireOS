0c86ac12e424967fa5789a7ac424d225baa9f2a8
Ukpaka <upaksabraham24@gmail.com>
2025-12-27 23:08:18 +0800

    Add placeholder API function for Vercel build

---
+++ api/index.mjs
@@ -0,0 +1,6 @@
+// Placeholder - will be overwritten by build script
+// This file exists so Vercel can detect the function before build
+export default async function handler() {
+  return new Response('Building...', { status: 503 });
+}
+
